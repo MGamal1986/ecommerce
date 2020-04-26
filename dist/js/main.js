cdc1978d9d981d2745df306542fded88d6a10b8b
MGamal1986 <elsayedmg@gmail.com>
2020-04-26 15:38:36 -0700

    continue in navbar

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,3 +1,24 @@
 "use strict";
 
-console.log('ahmed');
+// add scroll event for body
+// catch navigation element
+var navigation = document.querySelector('.navigation');
+window.addEventListener('scroll', function (e) {
+  if (document.documentElement.scrollTop > 350) {
+    navigation.classList.add('shad');
+  } else {
+    navigation.classList.remove('shad');
+  }
+}); // on focus increase width of search input
+// catch input 
+
+var searchInput = document.querySelector('.nav-header input');
+var searchClass = searchInput.parentElement; // add focus event
+
+searchInput.addEventListener('focus', function (e) {
+  searchClass.style.width = "450px";
+}); // add blur event
+
+searchInput.addEventListener('blur', function (e) {
+  searchClass.style.width = "240px";
+});
--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,25 @@
-console.log('ahmed');
+
+// add scroll event for body
+// catch navigation element
+const navigation = document.querySelector('.navigation');
+window.addEventListener('scroll',(e)=>{
+    if(document.documentElement.scrollTop > 350){
+        navigation.classList.add('shad');
+    }else{
+        navigation.classList.remove('shad');
+
+    }
+})
+
+// on focus increase width of search input
+// catch input 
+const searchInput = document.querySelector('.nav-header input');
+const searchClass = searchInput.parentElement;
+// add focus event
+searchInput.addEventListener('focus',(e)=>{
+    searchClass.style.width = "450px";
+});
+// add blur event
+searchInput.addEventListener('blur',(e)=>{
+    searchClass.style.width = "240px";
+});
