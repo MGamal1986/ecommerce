807c210bc7c81d875aa650554acba88b98a4cfa1
MGamal1986 <elsayedmg@gmail.com>
2020-05-10 21:06:58 +0200

    continue in new cart design

--- src/js/includes/cart.js
+++ src/js/includes/cart.js
@@ -1,93 +1,31 @@
-// // open cart page
-// // catch cart element
-// const cartElement = document.querySelector('.nav-header .user .cart');
-// // catch overlay layer
-// const overlay = document.querySelector('nav .overlay');
-// // catch cart contianer in overlay layer
-// const cartContainer = document.querySelector('nav .cart-container');
-
-// cartElement.addEventListener('click',(event)=>{
-//     overlay.classList.add('overhead');
-//     setTimeout(()=>{
-//         cartContainer.classList.add('appear');
-//     },10)
-    
-// });
-
-// // catch close x in cart container in samll screen
-// const cartCloseX = document.querySelector('nav .cart-container .close-x');
-// // add click event to close cart pagein samll screen
-
-// cartCloseX.addEventListener('click',(event)=>{
-//     cartContainer.classList.remove('appear');
-//     setTimeout(()=>{
-//         overlay.classList.remove('overhead');
-//     },400)
-// })
-
-// // add click event on overlay layer
-// overlay.addEventListener('click',(event)=>{
-//     if(!event.target.closest('.cart-container')){
-//         cartContainer.classList.remove('appear');
-//         setTimeout(()=>{
-//             overlay.classList.remove('overhead');
-//         },400)
-//     } 
-// })
-
-
-// // remove items from cart
-// // catch remove button
-// const removeBtn = document.querySelectorAll('.cart-container .cart-items .item .remove i');
-// removeBtn.forEach((btn)=>{
-//     btn.addEventListener('click',function(event){
-//         console.log('good selector');
-//         event.target.parentElement.parentElement.remove();
-//     })
-// })
-
-// // count items in cart
-// const cartItems = document.querySelectorAll('nav .cart-container .cart-items .item');
-// // after we add event for selecting product we will remove setInterval function
-// setInterval(()=>{
-//     let cartLength = cartItems.length;
-//     if(cartLength >= 6){
-//         cartContainer.classList.add('more-6');
-//     }
-// },300)
-
-// // increment and decrement items in cart
-// // catch input field
-// const itemInput = document.querySelector('nav .cart-container .item .number');
-// // catch button
-// const incrementBtn = document.querySelectorAll('nav .cart-container .item .fa-plus');
-// const decrementBtn = document.querySelectorAll('nav .cart-container .item .fa-minus');
-
-// // add increment event
-// incrementBtn.forEach((btn)=>{
-//     btn.addEventListener('click',(event)=>{
-//         event.target.previousElementSibling.value++
-//     })
-// })
-
-// // add decrement event
-// decrementBtn.forEach((btn)=>{
-//     btn.addEventListener('click',(event)=>{
-//         if(event.target.nextElementSibling.value > 0){
-//             event.target.nextElementSibling.value--
-//         }
-//     })
-// })
-
-// show cart on click on cart icon
-// catch cart icon in navbar
-const cartIcon = document.querySelector('.nav-header .user .cart');
-// catch cart element
-const cartContainer = document.querySelector('nav .cart-container');
-cartIcon.addEventListener('click',(event)=>{
-    cartContainer.classList.toggle('expand');
+// remove items form cart
+// catch close btn for each item
+const cartIconClose = document.querySelectorAll('.nav-header .user .cart .close-x');
+cartIconClose.forEach((close)=>{
+    close.onclick = (event)=>{
+        event.target.parentElement.parentElement.remove();
+    }
 })
 
+// add more than one items
+// catch increment and decrement button
+const incrementBtn = document.querySelectorAll('.nav-header .user .cart .item i.fa-plus');
 
+const decrementBtn = document.querySelectorAll('.nav-header .user .cart .item i.fa-minus');
+
+// add increment event
+incrementBtn.forEach((btn)=>{
+    btn.onclick = (event)=>{
+        event.target.previousElementSibling.textContent ++;
+    }
+})
+// add decrement event
+decrementBtn.forEach((btn)=>{
+    btn.onclick = (event)=>{
+        if(event.target.nextElementSibling.textContent > 0 ){
+            event.target.nextElementSibling.textContent --;
+        }
+    }
+})
 
 
