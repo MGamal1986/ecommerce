e40904713401c125798e8691b3d5e9cc0673bd76
MGamal1986 <elsayedmg@gmail.com>
2020-05-30 15:19:50 +0200

    finish sort by menu in shop page

--- src/js/includes/ShopSortBy.js
+++ src/js/includes/ShopSortBy.js
@@ -1,19 +1,35 @@
 import {addEvent} from './Functions.js';
 function ShopSortBy(){
     // catch list span
-    const listSpan = document.querySelector('.products .drop .menu .menu-head');
+    const listSpan = document.querySelector('.products .drop .menu .menu-head span');
     // catch sort by menu
     const sortMenu = document.querySelector('.products .drop .menu .menu-list');
+    // catch angle down menu
+    const angleMenu = document.querySelector('.products .drop .menu .menu-head i');
     // add click event to span list
     addEvent(listSpan,'click',(event)=>{
         sortMenu.classList.toggle('face');
-    })
+        angleMenu.classList.toggle('rotate');
+    });
+
+
     // catch sort by menu item
     const sortMenuItem = document.querySelectorAll('.products .drop .menu .menu-list .menu-item');
     sortMenuItem.forEach(item=>{
+        // add click event to open sort by menu and rotate angle menu
         addEvent(item,'click',(event)=>{
+            let elements = event.target.parentElement.children;
+            // we use for of as HTMLCollection doesnot have forEach function but it has [symbol.iterator] key
+            for(let el of elements){
+                el.classList.remove('active');
+                
+            }
             event.target.classList.toggle('active');
+            listSpan.textContent = event.target.textContent;
+            sortMenu.classList.toggle('face');
+            angleMenu.classList.toggle('rotate');
         })
-    })
+    });
+    // 
 }
 export default ShopSortBy;
