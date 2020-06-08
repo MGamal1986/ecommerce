af3110139734e2e5d30e2fd32adefa312982486a
MGamal1986 <elsayedmg@gmail.com>
2020-06-08 20:39:25 +0200

    add ion-RangeSlider library and finish price section in shop page

--- src/js/includes/RangeSlider.js
+++ src/js/includes/RangeSlider.js
@@ -1,6 +1,15 @@
-const $ = require('jquery');
+const JQuery = require('jquery');
+const rangeSlider = require('ion-rangeslider');
 
 
 export default function RangeSlider(){
-    $('.price input').fadeOut('slow');
+    JQuery("#example_id").ionRangeSlider({
+        type: "double",
+        grid: true,
+        min: 0,
+        max: 20000,
+        from: 200,
+        to: 800,
+        prefix: "$",
+    });
 }
